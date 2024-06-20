087e3eea1f81c91522dac21f5b671f47b1ce7a1b
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2024-06-20 07:09:32 +0200

    Clase 05

--- 02_code/conquermanager/todos/models.py
+++ 02_code/conquermanager/todos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(
+        'Nombre',
+        max_length=30
+    )
+    last_name = models.CharField(
+        'Apellidos',
+        max_length=30
+    )
+    age = models.IntegerField(
+        'Edad',
+        default=18,
+        help_text='Introduce tu edad en formato número'
+    )
+    dni = models.CharField(
+        'DNI',
+        max_length=9,
+        unique=True
+    )
