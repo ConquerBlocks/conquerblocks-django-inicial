7c122406f4830ef400b645f2aa67cd443292728f
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2024-06-26 13:22:24 +0200

    Archivos de la clase 7

--- 02_code/conquermanager/todos/models.py
+++ 02_code/conquermanager/todos/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Family(models.Model):
-    name = models.CharField(max_length=140)
+    name = models.CharField(
+        max_length=140,
+        unique=True
+    )
 
     def __str__(self):
         return f'{self.name}'
--- 02_code/conquermanager/utils.py
+++ 02_code/conquermanager/utils.py
@@ -1,4 +1,8 @@
-family1 = Family.objects.create(
+from todos.models import Family, Person
+
+bienve = Person.objects.get(dni='8874746484X')
+
+los_saez = Family.objects.create(
   name="Los Sáez"
 )
 
@@ -16,6 +20,13 @@ p1 = Person.objects.create(
   dni='8874746484X'
 )
 
+felipe = Person.objects.create(
+  first_name='Felipe',
+  last_name='Rodríguez',
+  dni='887474X'
+)
+
+
 angela = Person.objects.create(
   family=family1,
   first_name='Ángela',
@@ -32,3 +43,25 @@ luis = Person.objects.create(
 
 
 [family.name for family in bienve.family.all()]
+
+
+for person in Person.objects.all():
+    print(person)
+
+
+for family in Family.objects.all():
+    print(family.name)
+
+
+saez = Family.objects.get(name="Los Sáez")
+
+familias = Family.objects.all()
+
+person_18 = Person.objects.filter(age=55)
+for person in person_18:
+    print(person)
+
+person_18 = Person.objects.exclude(age=55)
+for person in person_18:
+    print(f'{person.first_name} - {person.age}')
+
