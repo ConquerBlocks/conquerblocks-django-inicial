3591d179dc0165e3753b13cc87c9285c2e067ce4
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2024-09-30 11:31:24 +0200

    clase 37

--- 02_code/biblioteca/books/urls/libro_url.py
+++ 02_code/biblioteca/books/urls/libro_url.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from books.views import (
   LibrosListView,
+  LibroDetailView
 )
 
 app_name = "libro"
 
 urlpatterns = [
     path("list/", LibrosListView.as_view(), name="list"),
+    path("detail/<pk>/", LibroDetailView.as_view(), name="detail"),
 ]
