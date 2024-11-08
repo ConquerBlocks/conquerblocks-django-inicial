4b91a2471dd47d432d3345c2a6d0bebeab320339
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2024-11-08 18:40:55 +0100

    Clase 10

--- 02_code/instagram/instagram/urls.py
+++ 02_code/instagram/instagram/urls.py
@@ -21,7 +21,8 @@ from django.urls import path
 from .views import HomeView, LoginView, RegisterView, ContactView, LegalView, logout_view, ProfileDetailView, ProfileUpdateView, ProfileListView
 from django.conf.urls.static import static
 from django.conf import settings
-from posts.views import PostCreateView
+from posts.views import PostCreateView, PostDetailView, like_post
+
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -33,6 +34,8 @@ urlpatterns = [
     path('profile/<pk>/', ProfileDetailView.as_view(), name='profile_detail'),
     path('profile/update/<pk>/', ProfileUpdateView.as_view(), name='profile_update'),
     path('posts/create/', PostCreateView.as_view(), name="post_create"),
+    path('posts/<pk>/', PostDetailView.as_view(), name="post_detail"),
+    path('posts/like/<pk>/', like_post, name="post_like"),
     path('legal/', LegalView.as_view(), name='legal'),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 02_code/instagram/posts/views.py
+++ 02_code/instagram/posts/views.py
@@ -4,8 +4,12 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from django.views.generic.detail import DetailView
 from .forms import PostCreateForm
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.decorators import login_required
+
 
 @method_decorator(login_required, name='dispatch')
 class PostCreateView(CreateView):
@@ -19,3 +23,21 @@ class PostCreateView(CreateView):
         messages.add_message(self.request, messages.SUCCESS, "Publicación creada correctamente.")
         return super(PostCreateView, self).form_valid(form)
 
+
+@method_decorator(login_required, name='dispatch')
+class PostDetailView(DetailView):
+    template_name = "posts/post_detail.html"
+    model = Post
+    context_object_name = 'post'
+  
+@login_required
+def like_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.user in post.likes.all():
+        messages.add_message(request, messages.INFO, "Ya no te gusta esta publicación.")
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+        messages.add_message(request, messages.INFO, "Te gusta esta publicación.")
+
+    return HttpResponseRedirect(reverse('post_detail', args=[pk]))
